b23adb99b016ecbcc68655ebc6e904fb1fee2160
John Renner <john@jrenner.net>
2018-02-28 14:47:28 -0800

    Fix bug with decoding declassify

--- interpreter/binary/decode.ml
+++ interpreter/binary/decode.ml
@@ -445,6 +445,8 @@ let rec instr s =
   | 0xc0 -> secret s
   | 0xc1 -> s32_classify_i32
   | 0xc2 -> s64_classify_i64
+  | 0xc3 -> i32_declassify
+  | 0xc4 -> i64_declassify
 
   | b -> illegal s pos b
 
@@ -548,8 +550,6 @@ and secret s =
   | 0xa7 -> s32_wrap_s64
   | 0xac -> s64_extend_s_s32
   | 0xad -> s64_extend_u_s32
-  | 0xc3 -> i32_declassify
-  | 0xc4 -> i64_declassify
   | b -> illegal s pos b
 
 let const s =
