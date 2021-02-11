bef563ae6789d5cd88117103672f91ee2da98448
Romy Tsoupidi <tsoupidi@kth.se>
2021-02-11 13:49:36 +0100

    Fix wait for portfolio.

--- interpreter/run_solvers.sh
+++ interpreter/run_solvers.sh
@@ -9,7 +9,7 @@ sed "s/(get-model)//" $filename > ${filename}.bool
 cvc4_pid=$!
 { z3 -smt2 MODEL=true $filename 1> $filename.z3.out 2> $filename.z3.err ; echo z3; } &
 z3_pid=$!
-{ yices-smt2 $filename 1> $filename.yices.out 2> $filename.yices.err ; echo yices; } &
+{ yices-smt2 $filename 1> $filename.yices.out 2> $filename.yices.err && echo yices; } &
 yices_pid=$!
 { boolector -m ${filename}.bool 1> $filename.boolector.out 2> $filename.boolector.err ; echo boolector; } &
 boolector_pid=$!
@@ -19,11 +19,19 @@ boolector_pid=$!
 #wait -n
 #wait -n
 #sleep 1
-wait $yices_pid || wait $z3_pic || wait $cvc4_pid || wait $boolector_pid 
+#{ wait $yices_pid && test $? -eq 0; } || 
+
+ret=1
+until [ $ret -eq 0 ]; do
+    wait -n
+    ret=$?
+done
+
+#wait $z3_pic || wait $cvc4_pid || wait $boolector_pid
 
 rm ${filename}.bool
 
-for i in $cvc4_pid $z3_pid $yices_pid $boolector_pid
+for i in $cvc4_pid $z3_pid $boolector_pid $yices_pid
 do
     pkill -9 -P $i &
     #    kill -9 $i
